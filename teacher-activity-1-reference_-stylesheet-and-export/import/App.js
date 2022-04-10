a09f25ed25450c69f736ee1097d54263a2a5cbd6
johanjince123 <89522174+johanjince123@users.noreply.github.com>
2022-04-09 19:48:46 -0500

    Add files via upload

---
+++ teacher-activity-1-reference_-stylesheet-and-export/import/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import SoundButton from './components/SoundButton';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <SoundButton />
+      </View>
+    );
+  }
+}
